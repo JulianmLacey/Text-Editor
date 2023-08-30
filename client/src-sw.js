55e1836ccfa5ccc80cd113fe2bfb4a568a20d04f
Julian Lacey <Julianmlacey@gmail.com>
2023-08-29 22:20:18 -0700

    Added Todos

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,30 +1,48 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
-
+const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
+const { CacheFirst } = require("workbox-strategies");
+const { registerRoute } = require("workbox-routing");
+const { CacheableResponsePlugin } = require("workbox-cacheable-response");
+const { ExpirationPlugin } = require("workbox-expiration");
+const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
+const { StaleWhileRevalidate } = require("workbox-strategies");
 precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
-  plugins: [
-    new CacheableResponsePlugin({
-      statuses: [0, 200],
-    }),
-    new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
-    }),
-  ],
+	cacheName: "page-cache",
+	plugins: [
+		new CacheableResponsePlugin({
+			statuses: [0, 200],
+		}),
+		new ExpirationPlugin({
+			maxAgeSeconds: 30 * 24 * 60 * 60,
+		}),
+	],
 });
 
 warmStrategyCache({
-  urls: ['/index.html', '/'],
-  strategy: pageCache,
+	urls: ["/index.html", "/"],
+	strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+	// Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
+	({ request }) => {
+		["style", "script", "worker"].includes(request.destination),
+			new StaleWhileRevalidate({
+				// Name of the cache storage.
+				cacheName: "asset-cache",
+				plugins: [
+					// https://developer.chrome.com/docs/workbox/reference/workbox-cacheable-response/#type-CacheableResponse
+					// https://developer.chrome.com/docs/workbox/modules/workbox-cacheable-response/
+					// If both statuses and headers are specified, then both conditions must be met for the Response to be considered cacheable.
+					// HTTP StatusCode=0 is associated with incomplete capture of a hit or page and often with a labeling of the hit as: request canceled ("ReqCancelled=Client" "ReqCancelled=Server" or "ReqCancelled=True").
+					new CacheableResponsePlugin({
+						statuses: [0, 200],
+					}),
+				],
+			});
+	}
+);
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,30 +1,104 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
+	return {
+		mode: "development",
+		entry: {
+			main: "./src/js/index.js",
+			install: "./src/js/install.js",
+			database: "./src/js/database.js",
+			editor: "./src/js/editor.js",
+			header: "./src/js/header.js",
+		},
+		output: {
+			filename: "[name].bundle.js",
+			path: path.resolve(__dirname, "dist"),
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: "./index.html",
+				title: "JATE",
+			}),
 
-    module: {
-      rules: [
-        
-      ],
-    },
-  };
+			// Injects our custom service worker
+			new InjectManifest({
+				swSrc: "./src-sw.js",
+				swDest: "src-sw.js",
+			}),
+			// Creates a manifest.json file.
+			new WebpackPwaManifest({
+				//fingerprints: false,
+				//inject: true,
+				name: "JATE",
+				short_name: "JATE",
+				description: "Online text editor",
+				background_color: "#225ca3",
+				theme_color: "#225ca3",
+				start_url: "./",
+				publicPath: "./",
+				icons: [
+					{
+						src: path.resolve("src/images/logo.png"),
+						sizes: [16, 96, 128, 192, 256, 384, 512],
+						destination: path.join("assets", "icons"),
+					},
+				],
+			}),
+
+			new WorkboxPlugin.GenerateSW({
+				// Do not precache images
+				exclude: [/\.(?:png|jpg|jpeg|svg)$/],
+
+				runtimeCaching: [
+					{
+						// Match any request that ends with .png, .jpg, .jpeg or .svg.
+						urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+
+						// Apply a cache-first strategy.
+						handler: "CacheFirst",
+
+						options: {
+							// Use a custom cache name.
+							cacheName: "images",
+
+							// Only cache 2 images.
+							expiration: {
+								maxEntries: 2,
+							},
+						},
+					},
+				],
+			}),
+		],
+		module: {
+			rules: [
+				{
+					test: /\.css$/i,
+					use: ["style-loader", "css-loader"],
+				},
+				{
+					test: /\.(png|svg|jpg|jpeg|gif)$/i,
+					type: "asset/resource",
+				},
+				{
+					test: /\.m?js$/,
+					exclude: /node_modules/,
+					// We use babel-loader in order to use ES6.
+					use: {
+						loader: "babel-loader",
+						options: {
+							presets: ["@babel/preset-env"],
+							plugins: ["@babel/plugin-proposal-object-rest-spread", "@babel/transform-runtime"],
+						},
+					},
+				},
+			],
+		},
+	};
 };
